b5df7537865ba313d6bf2a6011c6d4ee891daa08
Serhii Nesterchuk <s.nesterchuk27@gmail.com>
2024-09-17 19:12:29 +0200

    finis

--- js/7-main.js
+++ js/7-main.js
@@ -2,8 +2,7 @@ let h1 = document.querySelectorAll("h1")[0];
 
 h1.addEventListener("click", () => {
     h1.style.color = "green";
-});
-
-setInterval(() => {
+    setTimeout(() => {
     h1.style.color = "black";
-}, 5000);
+    }, 5000);
+});
